c4b0b682f0ffca46e58391bc4ecc7fde102746f0
Scotty Waggoner <ozzieorca@gmail.com>
2015-09-17 11:46:31 -0400

    Add modal to display transaction data for a specific month and category (cell).

--- src/app/components/tableGroup/tableGroup.directive.js
+++ src/app/components/tableGroup/tableGroup.directive.js
@@ -25,10 +25,39 @@
     return directive;
 
     /** @ngInject */
-    function TableGroupController() {
+    function TableGroupController($modal) {
       var vm = this;
       vm.isCollapsed = true;
       vm.expand = vm.expand === false ? vm.expand : true; //Default to true unless it is already set to false
+      vm.showMonthTransactions = showMonthTransactions;
+
+      function showMonthTransactions(transactions, date, category) {
+        $modal.open({
+          animation: true,
+          templateUrl: '/app/components/tableGroup/monthTransactions.modal.html',
+          controller: MonthTransactionsController,
+          controllerAs: 'monthTransactions',
+          bindToController: true,
+          resolve: {
+            transactions: function(){
+              return transactions;
+            },
+            date: function(){
+              return date;
+            },
+            category: function(){
+              return category;
+            }
+          }
+        });
+
+        function MonthTransactionsController(transactions, date, category, moment){
+          var vm = this;
+          vm.transactions = transactions;
+          vm.date = moment(date, 'YYYY-MM').format('x');
+          vm.category = category;
+        }
+      }
     }
   }
 })();
